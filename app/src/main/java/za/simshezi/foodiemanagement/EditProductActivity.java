139a96b5a2a9c9fd4d5eb2d0acef24fb590ff7c9
Simthembile-Shezi <s221517979@mandela.ac.za>
2023-09-24 19:21:06 +0200

    Working on adding ingredients

---
+++ app/src/main/java/za/simshezi/foodiemanagement/EditProductActivity.java
@@ -0,0 +1,17 @@
+package za.simshezi.foodiemanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class EditProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_product);
+    }
+
+    public void onUpdateProductClicked(View view) {
+    }
+}
